597c2e929211bb9de8459a04d5bb946df432274b
liiz-durang <lizeth.durang@gmail.com>
2024-05-13 23:30:34 -0600

    Replacement of categories in the applicaction form

--- src/config.ts
+++ src/config.ts
@@ -65,11 +65,8 @@ export const eas = {
 };
 
 export const impactCategories = {
-  ETHEREUM_INFRASTRUCTURE: { label: "Ethereum Infrastructure" },
-  OPEN_SOURCE: { label: "Web3 Open Source Software" },
-  COMMUNITY_EDUCATION: { label: "Web3 Community & Education" },
-  COLLECTIVE_GOVERNANCE: { label: "Collective Governance" },
-  OP_STACK: { label: "OP Stack" },
-  DEVELOPER_ECOSYSTEM: { label: "Developer Ecosystem" },
-  END_USER_EXPERIENCE_AND_ADOPTION: { label: "End user UX" },
+  DISMINUCION_BRECHA_GENERO: { label: "Disminución de Brecha de Género " },
+  ONBOARDING_NUEVOS_USUARIOS: { label: "Onboarding de Nuevos Usuarios" },
+  COMUNIDADES_LOCALES: { label: "Comunidades Locales" },
+  REFI: { label: "ReFi" },
 } as const;
