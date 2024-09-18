2268387551528afb8909cda32eea84779e989dbe
Crisgarner <@crisgarner>
2024-09-18 14:42:57 -0600

    fixed sizes

--- packages/interface/src/features/ballot/components/ProjectAvatarWithName.tsx
+++ packages/interface/src/features/ballot/components/ProjectAvatarWithName.tsx
@@ -23,14 +23,12 @@ export const ProjectAvatarWithName = ({
 
   return (
     <Component className="flex flex-1 items-center gap-4" href={`/projects/${id}`} tabIndex={-1}>
-      <ProjectAvatar rounded="full" size="sm" url={metadata.data?.bannerImageUrl} />
+      <ProjectAvatar  size="lg" url={metadata.data?.profileImageUrl} />
 
       <div>
         <div className="font-bold uppercase">{projects?.[0]?.name}</div>
 
         <div className="text-sm text-gray-400">
-          <p>{showDescription && (metadata.data?.bio ?? null)}</p>
-
           <p>{allocation > 0 && `Votes you have allocated: ${allocation}`}</p>
         </div>
       </div>
