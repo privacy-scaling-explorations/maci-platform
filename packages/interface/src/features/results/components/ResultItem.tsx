e21e5e47f1f8a4a2ace25aabe9e61260c5ab36a1
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2024-11-23 14:50:26 +0000

    feat: show amount to be distributed

--- packages/interface/src/features/results/components/ResultItem.tsx
+++ packages/interface/src/features/results/components/ResultItem.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useRound } from "~/contexts/Round";
+import { Tally__factory as TallyFactory } from "maci-platform-contracts/typechain-types";
 
 import { useProjectMetadata } from "~/features/projects/hooks/useProjects";
+import { publicClient } from "~/utils/accountAbstraction";
 
 import type { IRecipientWithVotes } from "~/utils/types";
+import { formatEther, formatUnits, Hex, parseEther } from "viem";
+import { type TallyData } from "maci-cli/sdk";
 
 export interface IResultItemProps {
   pollId: string;
@@ -14,6 +19,82 @@ export interface IResultItemProps {
 
 export const ResultItem = ({ pollId, rank, project }: IResultItemProps): JSX.Element => {
   const metadata = useProjectMetadata(project.metadataUrl);
+
+  const { getRoundByPollId } = useRound();
+
+  const roundData = useMemo(() => getRoundByPollId(pollId), [getRoundByPollId, pollId]);
+
+  const [amount, setAmount] = useState<string>("");
+
+  const [tallyData, setTallyData] = useState<TallyData | undefined>(undefined);
+
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_TALLY_URL + `/tally-${pollId}.json`)
+      .then((res) => res.json() as Promise<TallyData>)
+      .then((res) => {
+        setTallyData(res);
+      })
+      .catch(() => undefined);
+  }, [roundData])
+
+  useEffect(() => {
+    const getAmount = async () => {
+      if (!tallyData?.perVOSpentVoiceCredits || !roundData?.tallyAddress) {
+        console.log("no tally data or tally address");
+        return;
+      }
+
+      // 250k with 18 decimals
+      const budget = BigInt(parseEther("250000"));
+      console.log("budget", budget);
+
+      const totalSpent = await publicClient.readContract({
+        address: roundData.tallyAddress as Hex,
+        abi: TallyFactory.abi,
+        functionName: "totalSpent",
+      });
+
+      console.log("total spent", totalSpent);
+
+      const totalVotesSquares = (await publicClient.readContract({
+        address: roundData.tallyAddress as Hex,
+        abi: TallyFactory.abi,
+        functionName: "totalVotesSquares",
+      })) + 100n;
+
+      console.log("totalVotesSquares", totalVotesSquares);
+
+      // everyone got the same amount of voice credits = 1000
+      const maxVoiceCredits = BigInt(1000);
+
+      const voiceCreditFactor = BigInt(10e6) * BigInt(10e18) / maxVoiceCredits;
+
+      console.log("voiceCreditFactor", voiceCreditFactor);
+
+      const alphaPrecision = BigInt(10e18);
+
+      const contributions = totalSpent * voiceCreditFactor;
+
+      const alpha = ((budget - contributions) * alphaPrecision) / (voiceCreditFactor * (totalVotesSquares - totalSpent));
+
+      const quadratic = alpha * voiceCreditFactor * (BigInt(project.votes) * BigInt(project.votes));
+      const totalSpentCredits = voiceCreditFactor * BigInt(tallyData.perVOSpentVoiceCredits.tally[Number.parseInt(project.index, 10)]!);
+      const linearPrecision = alphaPrecision * totalSpentCredits;
+      const linearAlpha = alpha * totalSpentCredits;
+
+      const amount = (quadratic + linearPrecision - linearAlpha) / alphaPrecision;
+
+      console.log("amount", amount);
+
+      const finalAmount = amount > BigInt(parseEther("30000")) ? BigInt(parseEther("30000")) : amount;
+
+      console.log("final amount", formatUnits(finalAmount, 18));
+      setAmount(formatUnits(finalAmount, 18).toString());
+    };
+
+    // eslint-disable-next-line no-console
+    getAmount().catch(console.error);
+  }, [roundData, tallyData, setAmount]);
 
   useEffect(() => {
     if (metadata.data) {
@@ -39,7 +120,11 @@ export const ResultItem = ({ pollId, rank, project }: IResultItemProps): JSX.Ele
 
         <div className="mx-2 flex-1">{metadata.data?.name}</div>
 
-        <div className="w-24 flex-none text-end">{`${project.votes} votes`}</div>
+        {amount ? (
+          <div className="w-24 flex-none text-end">{`${amount} USD`}</div>
+        ) : (
+          <div className="w-24 flex-none text-end">{`${project.votes} votes`}</div>
+        )}
       </div>
     </Link>
   );
