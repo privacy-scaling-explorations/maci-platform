28ea7688de2f1561178f4bc36619ffa20e69fd8f
Kalidou Diagne <kld.diagne@gmail.com>
2025-02-18 14:10:43 +0100

    feat: design implementation progress

--- packages/interface/src/layouts/AppContainer.tsx
+++ packages/interface/src/layouts/AppContainer.tsx
@@ -2,15 +2,22 @@ import { type PropsWithChildren } from "react";
 
 import { cn } from "~/utils/classNames";
 
-type IAppContainerProps = PropsWithChildren<{ as?: React.ElementType; className?: string }>;
+type IAppContainerProps = PropsWithChildren<{ as?: React.ElementType; className?: string; fullWidth?: boolean }>;
 
 export const AppContainer = ({
   children = null,
   as: Component = "div",
   className = "",
+  fullWidth = false,
   ...props
 }: IAppContainerProps): JSX.Element => (
-  <Component className={cn("mx-auto flex w-full max-w-screen-2xl md:px-[30px]", className)} {...props}>
+  <Component
+    className={cn("mx-auto flex w-full", className, {
+      "max-w-screen-2xl": !fullWidth,
+      "max-w-full": fullWidth,
+    })}
+    {...props}
+  >
     {children}
   </Component>
 );
